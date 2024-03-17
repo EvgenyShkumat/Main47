b9ddf966186e1c656198a9c319fc7e1b6401c697
Евгений Шкумат <kotbmasle@gmail.com>
2024-03-18 00:42:26 +0300

    add and use neighbour_matrix_elemetns function

--- Project01/Logic.cpp
+++ Project01/Logic.cpp
@@ -1,3 +1,6 @@
+#include <string>
+using namespace std;
+
 int find_column_sum(int** matrix, int heigth, int column_index) {
 	int sum = 0;
 	
@@ -24,6 +27,30 @@ int find_column_max_sum_index(int** matrix, int heigth, int length) {
 	return max_index;
 }
 
-// 1 2 3
-// 4 5 6 
-// 7 8 9 
+string neighbour_matrix_elemetns(int** matrix, int heigth, int length) {
+	string msg = "";
+	int count = 0;
+	for (int i = 0; i < heigth; i++)
+	{
+		for (int j = 0; j < length; j++)
+		{
+			msg += to_string(*(*(matrix + i) + j)) + "(" + to_string(count) + " element): ";
+			if (i != 0) {
+				msg += to_string(*(*(matrix + i - 1) + j)) + " ";
+			}
+			if (i != heigth - 1) {
+				msg += to_string(*(*(matrix + i + 1) + j)) + " ";
+			}
+			if (j != 0) {
+				msg += to_string(*(*(matrix + i) + j - 1)) + " ";
+			}
+			if (j != length - 1) {
+				msg += to_string(*(*(matrix + i) + j  + 1)) + " ";
+			}
+			msg += "\n";
+			count++;
+		}
+	}
+
+	return msg;
+}
--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -5,12 +5,13 @@ string convert_to_string(int** matrix, int length, int heigth);
 void random_init(int** matrix, int heigth, int length, int min, int max);
 
 int find_column_max_sum_index(int** matrix, int heigth, int length);
+string neighbour_matrix_elemetns(int** matrix, int heigth, int length);
 
 int main() {
 	srand(time(NULL));
 
-	int heigth = 5;
-	int length = 6;
+	int heigth = 3;
+	int length = 3;
 
 	int** matrix = new int* [heigth];
 
@@ -21,8 +22,7 @@ int main() {
 
 	random_init(matrix, heigth, length, 0, 5);
 	cout << convert_to_string(matrix, length, heigth);
-	cout << "The index of column with the max sum is: " <<
-		find_column_max_sum_index(matrix, heigth, length);
+	cout << neighbour_matrix_elemetns(matrix, heigth, length);
 
 	for (int i = 0; i < heigth; i++)
 	{
